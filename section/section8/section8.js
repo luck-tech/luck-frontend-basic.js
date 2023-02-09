6473aef61d57216706c964e63d1783626e6ddcdf
luck-tech <luck@imai.am>
2023-02-09 18:32:09 +0900

    section8

--- section/section8/section8.js
+++ section/section8/section8.js
@@ -1,13 +1,53 @@
-const firstIndex = null;
-const countArrayLength = null;
+const firstIndex = (a) => {
+  return a[0];
+};
+const countArrayLength = (a) => {
+  if (Array.isArray(a) === true) {
+    if (a.length === 0) {
+      return "この配列は空配列です";
+    } else {
+      return `この配列は${a.length}個のデータを持っています`;
+    }
+  } else {
+    return "これは配列ではありません";
+  }
+};
 
-const extractArray = null;
+const extractArray = (a) => {
+  if (a.length >= 3) {
+    return [a[1], a[2]];
+  } else if (a.length === 2) {
+    return [a[1]];
+  } else {
+    return [];
+  }
+};
 
-const makeMark = null;
+const makeMark = (a, b) => {
+  const array = [];
+  for (let i = 0; i < b; i++) {
+    array.push(a);
+  }
+  return array;
+};
 
-const onlyString = null;
+const onlyString = (a) => {
+  const array = [];
+  for (let i = 0; i < a.length; i++) {
+    if (typeof a[i] === "string") {
+      array.push(a[i]);
+    }
+  }
+  return array;
+};
 
-const addNumber = null;
+const addNumber = (a) => {
+  const array = [];
+  for (let i = 0; i < a.length; i++) {
+    array.push(`${i + 1}: ${a[i]}`);
+  }
+  return array;
+};
 
 module.exports = {
   firstIndex,
