304b349b1b7316028f3eefd1b47ca7ae6838b1b0
luck-tech <luck@imai.am>
2023-02-09 15:13:09 +0900

    section6

--- section/section6/section6.js
+++ section/section6/section6.js
@@ -1,3 +1,17 @@
-const sum = null;
+const sum = (a, b) => {
+  let i,
+    ans = 0;
+  if (a <= b) {
+    for (i = 0; a + i <= b; i++) {
+      ans += a + i;
+    }
+    return ans;
+  } else {
+    for (i = 0; b + i <= a; i++) {
+      ans += b + i;
+    }
+    return ans;
+  }
+};
 
 module.exports = { sum };
