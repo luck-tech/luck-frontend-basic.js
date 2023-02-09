08635d130e246d6d2ce5810ef1aca741ffce5596
luck-tech <luck@imai.am>
2023-02-09 16:14:53 +0900

    section7

--- section/section7/section7.js
+++ section/section7/section7.js
@@ -1,9 +1,22 @@
-const character = null;
+const character = {
+  id: 1,
+  name: "スライム",
+  hp: 50,
+};
 
-const addMP = null;
+const addMP = (a) => {
+  character.mp = a;
+  return character;
+};
 
-const updateHP = null;
+const updateHP = () => {
+  character.hp = 35;
+  delete character.mp;
+  return character;
+};
 
-const LookUpName = null;
+const LookUpName = () => {
+  return character.name;
+};
 
 module.exports = { character, addMP, updateHP, LookUpName };
