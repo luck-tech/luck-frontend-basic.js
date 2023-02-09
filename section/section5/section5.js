741611bd741c8355293001f9cf8bddefbaa24c61
luck-tech <luck@imai.am>
2023-02-09 13:47:53 +0900

    section5

--- section/section5/section5.js
+++ section/section5/section5.js
@@ -1,3 +1,14 @@
-const score = null;
-
+const score = (a) => {
+  if (a === undefined) {
+    return "後日受験";
+  } else if (a < 0 || a > 100 || typeof a === "string") {
+    return "0以上100未満の数字を入れてください";
+  } else if (0 <= a && a <= 59) {
+    return "不合格";
+  } else if (60 <= a && a <= 79) {
+    return "追試";
+  } else {
+    return "合格";
+  }
+};
 module.exports = { score };
